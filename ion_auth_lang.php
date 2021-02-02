f755f339de1fa688d75b81687e9e4caa08c4b8a2
Shaibal Saha <sahashaibal22@gmail.com>
2021-02-02 12:35:09 +0600

    Translate the ion auth lang file fully

--- ion_auth_lang.php
+++ ion_auth_lang.php
@@ -5,74 +5,77 @@
 * Author: Ben Edmunds
 * 		  ben.edmunds@gmail.com
 *         @benedmunds
-*
+
+*File Author: Shaibal Saha
+* 		  sahashaibal22@gmail.com
+*         @shaibal13
 * Location: http://github.com/benedmunds/ion_auth/
 *
 * Created:  03.14.2010
 *
-* Description:  English language file for Ion Auth messages and errors
+* Description:  Burmese language file for Ion Auth messages and errors
 *
 */
 
 // Account Creation
-$lang['account_creation_successful'] 	  	 = 'Account Successfully Created';
-$lang['account_creation_unsuccessful'] 	 	 = 'Unable to Create Account';
-$lang['account_creation_duplicate_email'] 	 = 'Email Already Used or Invalid';
-$lang['account_creation_duplicate_username'] = 'Username Already Used or Invalid';
-$lang['account_creation_missing_default_group'] = 'Default group is not set';
-$lang['account_creation_invalid_default_group'] = 'Invalid default group name set';
+$lang['account_creation_successful'] 	  	 = 'အကောင့်ကိုအောင်မြင်စွာဖန်တီးပြီး';
+$lang['account_creation_unsuccessful'] 	 	 = 'အကောင့်ဖန်တီးရန်မရပါ';
+$lang['account_creation_duplicate_email'] 	 = 'အီးမေးလ်ရှိပြီးသားသို့မဟုတ်မမှန်ကန်သော';
+$lang['account_creation_duplicate_username'] = 'အသုံးပြုပြီးသားသို့မဟုတ်မမှန်ကန်သောသုံးစွဲသူအမည်';
+$lang['account_creation_missing_default_group'] = 'ပုံမှန်အုပ်စုကိုမသတ်မှတ်ပါ';
+$lang['account_creation_invalid_default_group'] = 'မမှန်ကန်သောပုံသေအုပ်စုအမည်';
 
 
 // Password
-$lang['password_change_successful'] 	 	 = 'Password Successfully Changed';
-$lang['password_change_unsuccessful'] 	  	 = 'Unable to Change Password';
-$lang['forgot_password_successful'] 	 	 = 'Password Reset Email Sent';
-$lang['forgot_password_unsuccessful'] 	 	 = 'Unable to Reset Password';
+$lang['password_change_successful'] 	 	 = 'စကားဝှက်ကိုအောင်မြင်စွာပြောင်းလဲပြီး';
+$lang['password_change_unsuccessful'] 	  	 = 'စကားဝှက်ကို ပြောင်းလဲ၍ မရပါ';
+$lang['forgot_password_successful'] 	 	 = 'အီးမေးလ်ပို့ပြီးစကားဝှက်ကိုပြန်လည်သတ်မှတ်';
+$lang['forgot_password_unsuccessful'] 	 	 = 'စကားဝှက်ကိုပြန်လည် သတ်မှတ်၍ မရပါ';
 
 // Activation
-$lang['activate_successful'] 		  	     = 'Account Activated';
-$lang['activate_unsuccessful'] 		 	     = 'Unable to Activate Account';
-$lang['deactivate_successful'] 		  	     = 'Account De-Activated';
-$lang['deactivate_unsuccessful'] 	  	     = 'Unable to De-Activate Account';
-$lang['activation_email_successful'] 	  	 = 'Activation Email Sent';
-$lang['activation_email_unsuccessful']   	 = 'Unable to Send Activation Email';
+$lang['activate_successful'] 		  	     = 'အကောင့် သက်ဝင်';
+$lang['activate_unsuccessful'] 		 	     = 'အကောင့်ကိုဖွင့ ်၍ မရပါ';
+$lang['deactivate_successful'] 		  	     = 'အကောင့်ဖျက်သိမ်းပြီး';
+$lang['deactivate_unsuccessful'] 	  	     = 'အကောင့်ဖယ်ရှား။ မရပါ';
+$lang['activation_email_successful'] 	  	 = 'တက်ကြွ အီးမေးလ်ပို့ပြီးပြီ';
+$lang['activation_email_unsuccessful']   	 = 'မရနိုင်ပါ ပို့ပါ တက်ကြွ အီးမေးလ်';
 
 // Login / Logout
-$lang['login_successful'] 		  	         = 'Logged In Successfully';
-$lang['login_unsuccessful'] 		  	     = 'Incorrect Login';
-$lang['login_unsuccessful_not_active'] 		 = 'Account is inactive';
-$lang['login_timeout']                       = 'Temporarily Locked Out.  Try again later.';
-$lang['logout_successful'] 		 	         = 'Logged Out Successfully';
+$lang['login_successful'] 		  	         = 'လော့ဂ်အင် အောင်မြင်ပါပြီ';
+$lang['login_unsuccessful'] 		  	     = 'မမှန်ကန်ပါ လော့ဂ်အင်';
+$lang['login_unsuccessful_not_active'] 		 = 'အကောင့်သည်လှုပ်ရှားမှုမရှိပါ';
+$lang['login_timeout']                       = 'ခေတ္တပိတ်ထား နောက်မှထပ်ကြိုးစားပါ.';
+$lang['logout_successful'] 		 	         = 'အောင်မြင်စွာထွက်လိုက်ပြီ';
 
 // Account Changes
-$lang['update_successful'] 		 	         = 'Account Information Successfully Updated';
-$lang['update_unsuccessful'] 		 	     = 'Unable to Update Account Information';
-$lang['delete_successful']               = 'User Deleted';
-$lang['delete_unsuccessful']           = 'Unable to Delete User';
+$lang['update_successful'] 		 	         = 'အကောင့်အချက်အလက်များကိုအောင်မြင်စွာအဆင့်မြှင့်တင်';
+$lang['update_unsuccessful'] 		 	     = 'အကောင့်အချက်အလက်ကိုမွမ်းမံ။ မရပါ';
+$lang['delete_successful']               = 'အသုံးပြုသူဖျက်သိမ်းသည်';
+$lang['delete_unsuccessful']           = 'အသုံးပြုသူကို ဖျက်၍ မရပါ';
 
 // Groups
-$lang['group_creation_successful']  = 'Group created Successfully';
-$lang['group_already_exists']       = 'Group name already taken';
-$lang['group_update_successful']    = 'Group details updated';
-$lang['group_delete_successful']    = 'Group deleted';
-$lang['group_delete_unsuccessful'] 	= 'Unable to delete group';
-$lang['group_delete_notallowed']    = 'Can\'t delete the administrators\' group';
-$lang['group_name_required'] 		= 'Group name is a required field';
-$lang['group_name_admin_not_alter'] = 'Admin group name can not be changed';
+$lang['group_creation_successful']  = 'အုပ်စု နေပြည်တော် အောင်မြင်ပါပြီ';
+$lang['group_already_exists']       = 'အုပ်စုနာမည်ပြီးသား';
+$lang['group_update_successful']    = 'အဖွဲ့၏အသေးစိတ်အချက်အလက်များ';
+$lang['group_delete_successful']    = 'အုပ်စုဖျက်ပြီးပါပြီ';
+$lang['group_delete_unsuccessful'] 	= 'အုပ်စုအား ဖျက်၍ မရပါ';
+$lang['group_delete_notallowed']    = 'အုပ်ချုပ်သူများ၏အဖွဲ့ကိုမဖျက်နိုင်ပါ';
+$lang['group_name_required'] 		= 'အုပ်စုအမည်သည်ဖြည့်ရန်လိုအပ်သည်';
+$lang['group_name_admin_not_alter'] = 'အက်မင်အုပ်စုအမည်ကို ပြောင်းလဲ၍ မရပါ';
 
 // Activation Email
-$lang['email_activation_subject']            = 'Account Activation';
-$lang['email_activate_heading']    = 'Activate account for %s';
-$lang['email_activate_subheading'] = 'Please click this link to %s.';
-$lang['email_activate_link']       = 'Activate Your Account';
+$lang['email_activation_subject']            = 'အကောင့် Activation';
+$lang['email_activate_heading']    = 'အကောင့်ဖွင့်ပါ %s';
+$lang['email_activate_subheading'] = 'ကျေးဇူးပြုပြီးဒီ link ကိုနှိပ်ပါ %s.';
+$lang['email_activate_link']       = 'သင်၏အကောင့်ကိုဖွင့်ပါ';
 
 // Forgot Password Email
-$lang['email_forgotten_password_subject']    = 'Forgotten Password Verification';
-$lang['email_forgot_password_heading']    = 'Reset Password for %s';
-$lang['email_forgot_password_subheading'] = 'Please click this link to %s.';
-$lang['email_forgot_password_link']       = 'Reset Your Password';
+$lang['email_forgotten_password_subject']    = 'လျှို့ဝှက်နံပါတ်မေ့နေခြင်း';
+$lang['email_forgot_password_heading']    = 'စကားဝှက်ကိုပြန်လည်သတ်မှတ်ပါ %s';
+$lang['email_forgot_password_subheading'] = 'ကျေးဇူးပြုပြီးဒီ link ကိုနှိပ်ပါ %s.';
+$lang['email_forgot_password_link']       = 'သင်၏စကားဝှက်ကိုပြန်လည်သတ်မှတ်ပါ';
 
 // New Password Email
-$lang['email_new_password_subject']          = 'New Password';
-$lang['email_new_password_heading']    = 'New Password for %s';
-$lang['email_new_password_subheading'] = 'Your password has been reset to: %s';
+$lang['email_new_password_subject']          = 'စကားဝှကိအသစ်';
+$lang['email_new_password_heading']    = 'စကားဝှက်အသစ် %s';
+$lang['email_new_password_subheading'] = 'သင်၏စကားဝှက်ကိုပြန်လည်သတ်မှတ်ပြီးဖြစ်သည်: %s';
