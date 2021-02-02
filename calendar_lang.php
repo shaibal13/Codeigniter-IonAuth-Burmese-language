022b9fd2b8d22a2ec87007a0a3892556e40e37ec
Shaibal Saha <sahashaibal22@gmail.com>
2021-02-02 13:00:02 +0600

    Adding Burmese for Hospital Project

---
+++ calendar_lang.php
@@ -0,0 +1,51 @@
+<?php
+
+$lang['cal_su']			= "တန";
+$lang['cal_mo']			= "မွန်း";
+$lang['cal_tu']			= "အင်္";
+$lang['cal_we']			= "ဗုဒ္";
+$lang['cal_th']			= "ကြာသ";
+$lang['cal_fr']			= "သေ";
+$lang['cal_sa']			= "စနေ";
+$lang['cal_sun']		= "တန";
+$lang['cal_mon']		= "မွန်း";
+$lang['cal_tue']		= "အင်္";
+$lang['cal_wed']		= "ဗုဒ္";
+$lang['cal_thu']		= "ကြာသ";
+$lang['cal_fri']		= "သေ";
+$lang['cal_sat']		= "စနေ";
+$lang['cal_sunday']		= "တနင်္ဂနွေနေ့";
+$lang['cal_monday']		= "မွန်းလွဲ";
+$lang['cal_tuesday']	= "အင်္ဂါနေ့";
+$lang['cal_wednesday']	= "ဗုဒ္ဓဟူးနေ့";
+$lang['cal_thursday']	= "ကြာသပတေးနေ့";
+$lang['cal_friday']		= "သောကြာနေ့";
+$lang['cal_saturday']	= "စနေနေ့";
+$lang['cal_jan']		= "ဇန်";
+$lang['cal_feb']		= "ဖေ";
+$lang['cal_mar']		= "မတ်";
+$lang['cal_apr']		= "ပြီလ";
+$lang['cal_may']		= "မေလ";
+$lang['cal_jun']		= "ဇွန်";
+$lang['cal_jul']		= "ဇူလို";
+$lang['cal_aug']		= "ဂုတ်";
+$lang['cal_sep']		= "စက်";
+$lang['cal_oct']		= "အောက်";
+$lang['cal_nov']		= "နိုဝင်";
+$lang['cal_dec']		= "ဒီဇင်";
+$lang['cal_january']	= "ဇန်နဝါရီ";
+$lang['cal_february']	= "ဖေဖော်ဝါရီလ";
+$lang['cal_march']		= "မတ်လ";
+$lang['cal_april']		= "ပြီလ";
+$lang['cal_mayl']		= "မေလ";
+$lang['cal_june']		= "ဇွန်";
+$lang['cal_july']		= "ဇူလိုင်လ";
+$lang['cal_august']		= "သြဂုတ်လ";
+$lang['cal_september']	= "စက်တင်ဘာ";
+$lang['cal_october']	= "အောက်တိုဘာ";
+$lang['cal_november']	= "နိုဝင်ဘာလ";
+$lang['cal_december']	= "ဒီဇင်ဘာ";
+
+
+/* End of file calendar_lang.php */
+/* Location: ./system/language/english/calendar_lang.php */
